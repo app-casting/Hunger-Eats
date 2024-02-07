a24580d9e585c49e4b3021ed1766ff97faa754f2
Kamal Pandey <kamalpandey9808@gmail.com>
2024-02-07 23:48:09 +0530

    Adding Crousel

--- src/Components/Items.jsx
+++ src/Components/Items.jsx
@@ -2,32 +2,31 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Items = () => {
-  return (
-    <div className="flex bg-gray-100 w-80 p-2 mt-4 ml-2 rounded-2xl border items-center justify-center  ">
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-auto">
-        {/* Iterate through items to create multiple links */}
-        {/* Assuming you have an array of item data */}
 
-        <Link to={"/"} className=" flex-col items-center  ">
-          <div className="w-60 h-60 relative  items-center justify-center ">
+   
+const quantity =[1,2,3,4,5]
+
+  return (
+    <div className="flex bg-gray-100 w-80 p-2 mt-4 ml-2 rounded-2xl border items-center justify-center">
+      <div className="">
+        <Link to={"/"} className="flex items-center justify-center">
+          <div className="w-60 h-60 relative">
             <img
               className="rounded-2xl w-full h-full object-cover"
               src={"/public/Photographs/Pizza.avif"}
               alt="pizza"
             />
-            <div className="absolute bottom-0 bg-white bg-opacity-75 w-full text-center">
+            <div className="absolute bottom-0 bg-white bg-opacity-75 w-full text-center ">
               <h2 className="font-bold">Margerita</h2>
-              <select
+              <select 
                 name="quantity"
-                className="w-16 border my-1 py-1 px-2 rounded-xl gap-1">
-                {""}
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5 ">5</option>
+                className="w-16 border my-1 py-1 px-2 rounded-xl gap-1"
+              >
+                {quantity.map((count)=>(
+                  <option key={"count"} value={count}>{count}</option>
+                ))}
               </select>
-              <span className="font-bold">$50</span>
+              <span className="font-bold ml-2">Total Price $100</span>
             </div>
           </div>
         </Link>
@@ -37,3 +36,4 @@ const Items = () => {
 };
 
 export default Items;
+
